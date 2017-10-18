e489dd12a3b6149f87593feb6096cfb30cd1c7bd
John Oliver <johnoliver021@gmail.com>
2017-10-18 13:06:45 -0200

    Update pilha-codigo.c

--- Estrutura de Dados/Atividade 2 - Pilhas/pilha-codigo.c
+++ Estrutura de Dados/Atividade 2 - Pilhas/pilha-codigo.c
@@ -25,7 +25,7 @@ int tamanho( pilha p){
     return p.topo + 1 ;
 }
 
-int empilha( pilha *p, telem valor){///função empilha pilha
+int empilha( pilha *p, telem valor){///funÃ§Ã£o empilha pilha
 
     if( cheia(*p))
         return 0;
@@ -45,7 +45,7 @@ void imprimir( pilha p){
         printf("%d", p.elemento[i]);
 }
 
-int topo( pilha p, telem *valor){///função q retorna 0 se a pilha estiver vazia e 1 se nao estiver e altera o valor do topo
+int topo( pilha p, telem *valor){///funÃ§Ã£o q retorna 0 se a pilha estiver vazia e 1 se nao estiver e altera o valor do topo
     if( vazia(p))
         return 0;
     else {
@@ -54,7 +54,7 @@ int topo( pilha p, telem *valor){///função q retorna 0 se a pilha estiver vazia 
     }
 }
 
-int desempilha( pilha *p, telem *valor){///função desempilhar retorna 0 se vazia e 1 se nao e modifica os valores do topo e do valor
+int desempilha( pilha *p, telem *valor){///funÃ§Ã£o desempilhar retorna 0 se vazia e 1 se nao e modifica os valores do topo e do valor
     if( vazia(*p))
         return 0;
     else {
@@ -64,7 +64,7 @@ int desempilha( pilha *p, telem *valor){///função desempilhar retorna 0 se vazia
 }
 ///Outra forma de se inverter uma pilha
 /*
-void inverter2(pilha *p, int vl){///Função q inverte os valores da pilha
+void inverter2(pilha *p, int vl){///FunÃ§Ã£o q inverte os valores da pilha
 int i, aux;
 for (i=0; i< vl-i-1; i++){
     aux = p->elemento[i];
@@ -74,12 +74,12 @@ for (i=0; i< vl-i-1; i++){
 
 }
 */
-void inverter(pilha *p, int vl){///função q inverte os valores de uma pilha
+void inverter(pilha *p, int vl){///funÃ§Ã£o q inverte os valores de uma pilha
 typedef struct{
 int elementos[MAX];
 int topo;
 
-}tpilha;///inicialização das pilhas
+}tpilha;///inicializaÃ§Ã£o das pilhas
 tpilha tem[2];
 int i, aux;
 for(i=0; i<2; i++){
@@ -104,7 +104,7 @@ while(tem[1].topo >-1){
 
 
 }
-void esvaziar (pilha *p){///função q serve pra esvaziar a pilha
+void esvaziar (pilha *p){///funÃ§Ã£o q serve pra esvaziar a pilha
 while(p->topo!=-1 ){
     p->elemento[p->topo--];
 
@@ -124,17 +124,17 @@ int i;
 int aux;
 while (p2->topo >-1){
     desempilha(p2, &aux);///desempilha os elementos de p2 para uma auxiliar, supondo , o valor desempilhado do p2 e 60
-    empilha(&tem, aux);///logo 60 e empilhado na pilha auxiliar, logo pilha auxiliar = 60, 50, 40 e p2 é zerada
+    empilha(&tem, aux);///logo 60 e empilhado na pilha auxiliar, logo pilha auxiliar = 60, 50, 40 e p2 Ã© zerada
 
     }
 while(tem.topo > -1){
-    desempilha(&tem, &aux);///desempilha os elementos da pilha auxiliar para a pilha que se deseja receber a concatenação
+    desempilha(&tem, &aux);///desempilha os elementos da pilha auxiliar para a pilha que se deseja receber a concatenaÃ§Ã£o
     empilha(p, aux);///se temporia estava com 60,50,40 e a pillha 1 com 10,20,30 logo a pilha 1 vai ficar : 10, 20, 30, 40, 50, 60
 }
 
 
 }
-void decToBin(int vl){///função q converte numero decimal para binario
+void decToBin(int vl){///funÃ§Ã£o q converte numero decimal para binario
 typedef struct{
 int elementos[50];
 int topo;
@@ -149,8 +149,8 @@ inicializa(&pt[i]);
 
 while((vl != 0) &&(vl !=1)){///enquanto o valor do inicio for diferente de 1 e de 0
       aux = vl % 2;///auxiliar recebe o resto da divisao do valor por 2
-      vl = vl/2;///o valor se divide por 2 para poder continuar a transformação
-      empilha(&pt[0], aux);///a primeira pilha vai recebendo os valores do resto da divisão
+      vl = vl/2;///o valor se divide por 2 para poder continuar a transformaÃ§Ã£o
+      empilha(&pt[0], aux);///a primeira pilha vai recebendo os valores do resto da divisÃ£o
       t++;
       }
     empilha(&pt[0], vl);///empilha o primeiro valor do numero binario na primeira pilha
@@ -165,7 +165,7 @@ while((vl != 0) &&(vl !=1)){///enquanto o valor do inicio for diferente de 1 e d
         printf("%d", pt[1].elementos[i]);
     }
 }
-int palindromo(int *valor, int t){///função verifica se uma string é palindroma
+int palindromo(int *valor, int t){///funÃ§Ã£o verifica se uma string Ã© palindroma
 typedef struct{
 int elementos[100];
 int top;
@@ -173,17 +173,17 @@ int top;
 }tpilha;
 tpilha pt[3];
 
-int i, tam2 =0, aux, flag=0;///se o flag incrementar e porq a função nao e palindroma
+int i, tam2 =0, aux, flag=0;///se o flag incrementar e porq a funÃ§Ã£o nao e palindroma
 for(i=0; i<3; i++){
     inicializa(&pt[i]);
 }
 for(i=0; i<t ; i++){
-if((valor[i] >58) &&(valor[i]<=126)){///verificacao pra retirar espacos em branco e numero e alguns caracteres especiais de acordo com sua numeração na tabela ASC
+if((valor[i] >58) &&(valor[i]<=126)){///verificacao pra retirar espacos em branco e numero e alguns caracteres especiais de acordo com sua numeraÃ§Ã£o na tabela ASC
     if((valor[i]>=65) &&(valor[i]<=90)){///verificacao de case sensitive
        valor[i]+=32;/// se o char for maiusculo ele fica minusculo pra nao destinguir maiusculas e minusculas
        }
-empilha(&pt[0], valor[i]);///se os criterios acima forem satisfeitos o valor dessa verificação e empilhado na p1
-empilha(&pt[1], valor[i]);///se os criterios acima forem satisfeitos o valor dessa verificação e empilhado na p2
+empilha(&pt[0], valor[i]);///se os criterios acima forem satisfeitos o valor dessa verificaÃ§Ã£o e empilhado na p1
+empilha(&pt[1], valor[i]);///se os criterios acima forem satisfeitos o valor dessa verificaÃ§Ã£o e empilhado na p2
 tam2++;
    }
 
@@ -196,7 +196,7 @@ for(i=0; i<tam2; i++){///desempilha a primeira pilha e empilha para uma segunda 
 }
 for(i=0; i<tam2; i++){
 
-        if(pt[2].elementos[i]!=pt[0].elementos[i]){///se algum valor for diferente, flag é incrementado, logo nao é palindromo
+        if(pt[2].elementos[i]!=pt[0].elementos[i]){///se algum valor for diferente, flag Ã© incrementado, logo nao Ã© palindromo
             flag++;
         }
 
@@ -206,6 +206,24 @@ if(flag==0){
 }else{
     return 0;
 
+}
+
+}
+int clonar (pilha p, pilha *clone){
+int aux;
+pilha tp;
+inicializa(clone);
+inicializa(&tp);
+
+while (p.topo!=-1){
+    aux = p.elemento[p.topo];
+    p.topo--;
+    empilha(&tp, aux);
+
+}
+while (tp.topo!=-1){
+    desempilha(&tp, &aux);
+    empilha(clone, aux);
 }
 
 }
