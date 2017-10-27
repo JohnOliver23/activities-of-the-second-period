141fa43f9ef1aff16e6acc38f1d20607b2312c28
John Oliver <johnoliver021@gmail.com>
2017-10-27 02:10:50 -0200

    Update exam.js

--- JavaScript/JSON/exam/exam.js
+++ JavaScript/JSON/exam/exam.js
@@ -14,44 +14,38 @@ function avg(students){
   return value /3
 }
 
-function min(students, n =1){
-   let result = []
-  //ORDENANDO ESTUDANTES
- students.sort( function (a, b) {
-  if (a.grade > b.grade) {
-    return 1
-  }
-  if (a.grade < b.grade) {
-    return -1
-  }
-  // a must be equal to b
-  return 0
-      
-    })//FIM DA ORDENAÇÃO
-  
-  //GERANDO ARRAY RESULTADO
-  if(n>1){
-  for(let i = 0; i < n; i++){
-   
+function min(students, n=1){
+let result = []
+const verific = ((a, b) =>{
+  return a.grade-b.grade//ordena os valores do menor pro maior , se a for maior q b , a passa pro indice de b e b pro índice de a
+})
+students.sort(verific)
+if(n > 1){
+  for(let i=0; i<n; i++){
     result.push(students[i].grade)
-    }
-  }else{
-    result = students[0].grade
   }
-  
-  
-  
+}else{
+  result = students[0].grade
+}
   return result
 }
 
-function max(students){
-  var valueMax = students['0'].grade
-  for (let value of Object.keys(students)){
-    if(students[value].grade > valueMax ){
-      valueMax = students[value].grade
+
+function max(students, n=1){
+  let valueMax = []
+  const verific = ((a, b)=> {
+    return b.grade-a.grade//ordena os valores do maior pro menor => se a for menor que b , a passa pro índice de a e a pro índice de b
+  })
+  students.sort(verific)
+  if(n >1){
+    for(i=0; i<n; i++){
+      valueMax.push(students[i].grade)
     }
+  }else{
+    valueMax = students[0].grade
   }
   return valueMax
+
 }
 function lt(students, point){
   let value = students.filter(x => x.grade < point).map(x => x.grade)
@@ -59,29 +53,25 @@ function lt(students, point){
 }
 
 function gt(students, point){
+  const verific = ((a, b)=> {
+    return a - b
+  })
   let value = students.filter(x => x.grade > point).map(x => x.grade)
+  value.sort(verific)//ordena os valores gerados da função acima, do menor para o maior
   return value
 }
-
-
-
 /*
 console.log(grade(studend1, weight, answer)) //=> 4
 console.log(grade(studend2, weight, answer)) //=> 8
 */
 
-students = [
 
-  {"student": "Sicrano", "grade": 5},
-  {"student": "Beltrano", "grade": 7},
-  {"student": "Fulano", "grade": 10},
-]
 module.exports = {grade, avg, min, max, lt, gt}
 
 //console.log(avg(students))                   //=> 7.333333333333333
 //console.log(min(students))                   //=> 5
 //console.log(min(students, 2))                //=> [5, 7]
 //console.log(max(students))                   //=> 10
-//console.log(max(students))                   //=> 10
+//console.log(max(students, 2))                //=> [10, 7]
 //console.log(lt(students, 6))                 //=> [5]
 //console.log(gt(students, 6))                 //=> [7, 10]
