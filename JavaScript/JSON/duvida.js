5c96971504304389919b613ff52235bfa84a4b3c
John Oliver <johnoliver021@gmail.com>
2017-10-27 01:15:26 -0300

    Add files via upload

---
+++ JavaScript/JSON/duvida.js
@@ -0,0 +1,12 @@
+var items = [
+  { name: 'Edward', value: 21 },
+  { name: 'Sharpe', value: 37 },
+  { name: 'And', value: 45 },
+  { name: 'The', value: -12 },
+  { name: 'Magnetic' },
+  { name: 'Zeros', value: 37 }
+];
+items.sort((a, b) => {
+  return a.value - b.value
+});
+console.log(items)
