d76b4d2d272c9ba5e534ced2d2928e8d1bc1250a
John Oliver <johnoliver021@gmail.com>
2017-12-14 22:42:02 -0200

    Add files via upload

---
+++ JavaScript/Exercism/twofer/twofer.js
@@ -0,0 +1,8 @@
+class TwoFer{
+  twoFer(name = "you"){
+    return `One for ${name}, one for me.`
+  }
+}
+  var twoFer = new TwoFer();
+  //console.log(twoFer.twoFer('Alice'))
+module.exports = TwoFer
